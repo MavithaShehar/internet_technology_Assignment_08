a098dd15d47dd60b799192391c1086abb303b865
MavithaShehar <mavithashehar@gmail.com>
2023-11-01 15:40:25 +0530

    all git hub push bug fixed

--- case_02/assets/js/script.js
+++ case_02/assets/js/script.js
@@ -1,5 +1,5 @@
-console.log("Hello World!");
 
+console.log("Hello World!");
 let array = ["red" , "green" , "blue" , "yellow" , "pink" , "deeppink"];
 
 let runningList = $(".middle_box");
@@ -8,7 +8,6 @@ const indexing = () => {
     for (let i = 0; i < runningList.length; i++) {
         runningList.eq(i).css("background" , array[i]);
     }
-
     array.unshift(array.pop());
 }
 setInterval(indexing , 500);
