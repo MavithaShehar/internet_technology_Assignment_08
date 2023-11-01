2799f6d1fd78ec05a78c985b8f73e92f7db9734e
MavithaShehar <mavithashehar@gmail.com>
2023-11-01 15:32:39 +0530

    all git hub push bug fixed

--- case_02/assets/js/script.js
+++ case_02/assets/js/script.js
@@ -1,89 +1,15 @@
+console.log("Hello World!");
 
-let round = 1;
-const lightsWork = () => {
+let array = ["red" , "green" , "blue" , "yellow" , "pink" , "deeppink"];
 
-    switch (round) {
+let runningList = $(".middle_box");
 
-        case 1: {
-
-            $(".middle_box").eq(0).css("background", "green");
-            $(".middle_box").eq(1).css("background", "blue");
-            $(".middle_box").eq(2).css("background", "yellow");
-            $(".middle_box").eq(3).css("background", "lightpink");
-            $(".middle_box").eq(4).css("background", "plum");
-            $(".middle_box").eq(5).css("background", "red");
-            round++;
-            break
-
-        }
-
-        case 2: {
-
-            $(".middle_box").eq(1).css("background", "green");
-            $(".middle_box").eq(2).css("background", "blue");
-            $(".middle_box").eq(3).css("background", "yellow");
-            $(".middle_box").eq(4).css("background", "lightpink");
-            $(".middle_box").eq(5).css("background", "plum");
-            $(".middle_box").eq(0).css("background", "red");
-            round++;
-            break
-
-        }
-
-        case 3: {
-
-            $(".middle_box").eq(2).css("background", "green");
-            $(".middle_box").eq(3).css("background", "blue");
-            $(".middle_box").eq(4).css("background", "yellow");
-            $(".middle_box").eq(5).css("background", "lightpink");
-            $(".middle_box").eq(0).css("background", "plum");
-            $(".middle_box").eq(1).css("background", "red");
-            round++;
-            break
-
-        }
-        case 4: {
-
-            $(".middle_box").eq(3).css("background", "green");
-            $(".middle_box").eq(4).css("background", "blue");
-            $(".middle_box").eq(5).css("background", "yellow");
-            $(".middle_box").eq(0).css("background", "lightpink");
-            $(".middle_box").eq(1).css("background", "plum");
-            $(".middle_box").eq(2).css("background", "red");
-            round++;
-            break
-
-        }
-        case 5: {
-
-            $(".middle_box").eq(4).css("background", "green");
-            $(".middle_box").eq(5).css("background", "blue");
-            $(".middle_box").eq(0).css("background", "yellow");
-            $(".middle_box").eq(1).css("background", "lightpink");
-            $(".middle_box").eq(2).css("background", "plum");
-            $(".middle_box").eq(3).css("background", "red");
-            round++;
-            break
-
-        }
-
- case 6: {
-
-            $(".middle_box").eq(5).css("background", "green");
-            $(".middle_box").eq(0).css("background", "blue");
-            $(".middle_box").eq(1).css("background", "yellow");
-            $(".middle_box").eq(2).css("background", "lightpink");
-            $(".middle_box").eq(3).css("background", "plum");
-            $(".middle_box").eq(4).css("background", "red");
-            round=1;
-            break
-
-        }
-
-
+const indexing = () => {
+    for (let i = 0; i < runningList.length; i++) {
+        runningList.eq(i).css("background" , array[i]);
     }
 
-
+    array.unshift(array.pop());
 }
 
-setInterval(lightsWork, 600);
+setInterval(indexing , 500);
